fc04c936f4e63dcf693a173f5fb0efa1ae69e033
idamariasofie <>
2023-11-12 19:48:17 +0000

    Create validate_data functions for size and quantity. Fix validate_juice_data to make sure it checks for valid data and raise error value if not

--- run.py
+++ run.py
@@ -22,10 +22,32 @@ def get_juice_selection():
         print("Then press Enter when you are ready.\n")
 
         juice_selection = input("Enter your order here:\n")
-        if validate_data(juice_selection):
+        if validate_juice_data(juice_selection):
             print("Thanks for your order")
             break
     return juice_selection
+
+def validate_juice_data(values):
+    """
+    This function checks if the values provided by the user, in the functions
+    get_juice_selection meet the requirements in the validate_juice_data.
+    Also validates if the format is correct. If any of the requirements is not
+    fulfilled it throws an error to inform the user.
+    """
+    try:
+        if len(values) != 1:
+            raise ValueError(f"Please enter 1 value you entered {len(values)}")
+
+        juice_selection = int(values[0])
+
+        if 1 <= juice_selection <= 5:
+            return True
+        else:
+            raise ValueError(
+                f"Please enter a number (1-5)\n you entered {len(values)}")
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again\n")
+        return False
 
 def get_size_selection():
     """
@@ -36,10 +58,24 @@ def get_size_selection():
         print("Then press Enter when you are ready.\n")
 
         size_selection = input("Enter your order here:\n")
-        if validate_data(size_selection):
+        if validate_size_data(size_selection):
             print("Thanks for your order")
             break
     return size_selection
+
+def validate_size_data(values):
+    """
+    This function checks if the values provided by the user, in the functions
+    get_size_selection meet the requirements in the validate_size_data.
+    Also validates if the format is correct. If any of the requirements is not
+    fulfilled it throws an error to inform the user.
+    """
+    try:
+        if len(values) != 1:
+            raise ValueError(f"Please enter 1 value you entered {len(values)}")
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
 
 def get_quantity():
     """
@@ -50,24 +86,29 @@ def get_quantity():
         print("Then press Enter when you are ready.\n")
 
         quantity = input("Enter your order here:\n")
-        if validate_data(quantity):
+        if validate_quantity_data(quantity):
             print("Thanks for your order")
             break
     return quantity
 
-def validate_data(values):
+def validate_quantity_data(values):
     """
     This function checks if the values provided by the user, in the functions
-    get_juice_selection, get_size_selection and get_quanity meet the
-    requirements in the values_required. Also validates if the format
-    is correct. If any of the requirements is not fulfilled it throws
-    an error to inform the user.
+    get_quanity function meet the requirements in the validate_quantity_data.
+    Also validates if the format is correct. If any of the requirements is not
+    fulfilled it throws an error to inform the user.
     """
-    print(values)
-        
-
+    try:
+        quantity = int(values)
+        if 1 <= quantity <= 10:
+            return True
+        else:
+            raise ValueError(
+                f"Please enter a number (1-5)\n you entered {quantity}")
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again\n")
+        return False
 
 get_juice_selection()
 get_size_selection()
 get_quantity()
-validate_data()
